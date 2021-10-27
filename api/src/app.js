e190cbc5052bcacacf203e8618883953bf50351a
Marco Galvan <elmacro11@gmail.com>
2021-10-27 19:17:11 -0300

    Configurar routes LOGIN

--- api/src/app.js
+++ api/src/app.js
@@ -1,29 +1,35 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+const loginRouter = require("./routes/login");
 
-require('./db.js');
+require("./db.js");
 
 const server = express();
 
-server.name = 'API';
+server.name = "API";
 
-server.use(express.json())
+server.use(express.json());
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-server.use('/', routes);
+server.use("/", routes);
+server.use("/login", loginRouter);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
