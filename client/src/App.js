d80471bf43bb9b5258134d79e57ef8487ab948c3
IanDiaz27 <iandiaz270601@gmail.com>
2021-10-27 13:11:18 -0300

    add Controlers & FormPacientCreate

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router';
+import FormPacientCreate from './components/FormPacientCreate/FormPacientCreate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route path='/register/pacient' component={FormPacientCreate}/>
     </div>
   );
 }
