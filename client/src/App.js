06e03a2fd0a9ced84a64a46028fc4ab5c9f891c1
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-10-28 00:06:19 -0400

    Ruta Seleccion tipo de formulario

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,13 @@ import { Route } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./components/Login/Login";
 import FormPacientCreate from "./components/FormPacientCreate/FormPacientCreate";
+import SelectorRegisterForm from "./pages/SelectorRegisterForm";
+
 function App() {
   return (
     <div>
       <Route path="/register/pacient" component={FormPacientCreate} />
+      <Route path="/register/form" component={SelectorRegisterForm} />
       <Route path="/login" component={LoginPage} />
       <Route path="/" component={LandingPage} />
     </div>
