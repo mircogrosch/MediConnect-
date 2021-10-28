6f3b689f6033e5e0db9a852261683824980d2559
Marco Galvan <elmacro11@gmail.com>
2021-10-28 18:30:48 -0300

    fallo passport

--- api/src/passport/local-auth.js
+++ api/src/passport/local-auth.js
@@ -10,3 +10,21 @@ passport.deserializeUser(async (dni, done) => {
   let user = await Person.findByPk(dni);
   done(null, user);
 });
+
+passport.use(
+  "local-login",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      let user = await Person.findOne({
+        where: {
+          email: email,
+        },
+      });
+    }
+  )
+);
