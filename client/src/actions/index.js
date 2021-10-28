619add6b7cd86aa386138b575b3e037dec90fe0d
mircogrosch <m1rc0.mg95@gmail.com>
2021-10-28 00:15:17 -0300

    refactor(actions/index.js) se agrega un request de tipo post al servidor, para el login de usuario

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import type from './types.js'
-const URL= 'localhost:3001/'
+const URL= 'localhost:3001'
 export const getUser=(user)=> { 
     return async function(dispatch){
-         const response = await axios.get(`${URL}login`); 
+         const response = await axios.post(`${URL}/login`,user); 
          dispatch({type:type.GET_USER_LOGIN, payload:response.data})
     }
 }
