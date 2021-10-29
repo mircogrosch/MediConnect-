c1729c4f10e96a4a8cb67d6688ff12140e140dee
mircogrosch <m1rc0.mg95@gmail.com>
2021-10-29 14:19:09 -0300

    se crea carpeta

---
+++ client/src/components/Home/Cards/ContainerCards.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Grid} from '@mui/material'
+import CardHome from './CardHome.jsx'
+
+function ContainerCards({cardInfo}) {
+    return (
+        <div>
+            <Grid container rowSpacing={10} columnSpacing={6} direction="row" justifyContent="center" alignItems="center" style={{marginTop:""}}>
+                {
+                   Array.isArray(cardInfo) ? cardInfo.map((info) =>(
+                        <Grid item> 
+                            <CardHome title={info.title} icon={info.icon} />
+                        </Grid> 
+                    )
+                ) :  <h1> No se recibio la informacion correspondiente</h1>
+                }
+            </Grid>
+        </div>
+    )
+}
+
+export default ContainerCards
