f621b116c2629e1b2862a8b93d6e969fd067cc29
unknown <robolfba@gmail.com>
2021-10-29 10:59:22 -0300

    refactor(client/src/components/FormProfesionalCreate): Se agrega carpeta para el componente form del profesional. Se integra el componente Signature a esta carpeta

---
+++ client/src/components/FormProfesionalCreate/styles.js
@@ -0,0 +1,10 @@
+import {makeStyles} from '@material-ui/core'
+const useStyles = makeStyles({
+    root:{ 
+        backgroundColor:"#E0F2F1"
+    },
+    textField: { 
+        marginBottom:15, 
+    },
+})
+export default useStyles;
