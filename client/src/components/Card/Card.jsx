5ac8f5af0ed84034caf7d579170359cba6eba752
IanDiaz27 <iandiaz270601@gmail.com>
2021-10-29 11:03:01 -0300

    feat(client/components/Card): Cree el componente de card

---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,68 @@
+import { Box, Icon, Typography } from "@material-ui/core";
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { AccountCircle } from "@mui/icons-material";
+
+const MyIcon = styled(Icon)({
+    display: 'contents'
+})
+
+const MyType = styled(Typography)({
+    display: 'inline',
+})
+
+const MyBox = styled(Box)({
+    display: 'inline-flex',
+    flexDirection: 'column',
+    width: '500px',
+    height: '180px',
+    margin: '30px',
+    borderRadius: '10px',
+})
+
+const MyBox2 = styled(Box)({
+    color: 'black',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '500px',
+    height: '200px',
+    borderRadius: '10px',
+})
+
+const MyBox3 = styled(Box)({
+    color: '#525752',
+    display: 'inline-flex',
+    fontSize: '20px',
+    flexDirection: 'column',
+    marginLeft: '120px',
+    marginTop: '-20px',
+    width: '500px',
+    height: '200px',
+    borderRadius: '10px',
+})
+
+const MyProfile = styled(AccountCircle)({
+    width: '90px',
+    height: '90px',
+    margin: '10px'
+})
+
+const Card = () => {
+    return(
+        <MyBox sx={{backgroundColor: '#80cbc4'}}>
+            <MyBox2>
+                <MyIcon><MyProfile/></MyIcon>
+                <MyType variant='h4'><b>Dr</b> nombreDelDoctor</MyType>
+            </MyBox2>
+            <MyBox3>
+                <Box sx={{marginBottom: '10px'}}>
+                    <Typography variant='body'><b>Fecha:</b> dd/mm/aaaa</Typography>
+                </Box>
+                <Typography variant='body'><b>Tipo:</b> TipoDeConsulta</Typography>
+            </MyBox3>
+        </MyBox>
+    )
+}
+
+export default Card
