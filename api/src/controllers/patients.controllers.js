a9a39c1ef7254a1fb9ae896ae7471dfcab47940e
Marco Galvan <elmacro11@gmail.com>
2021-10-28 22:22:54 -0300

    Completo encriptacion de password, falta traer obra social, toque el archivo patient.controller

--- api/src/controllers/patients.controllers.js
+++ api/src/controllers/patients.controllers.js
@@ -1,5 +1,9 @@
 const { Person, Patient } = require("../db");
-
+const bcrypt = require("bcrypt");
+//Encriptar password
+function encryptPassword(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
 const createPatient = async (req, res) => {
   const {
     dni,
@@ -21,7 +25,7 @@ const createPatient = async (req, res) => {
         address,
         imageProfile,
         email,
-        password,
+        password: encryptPassword(password),
         rol,
       },
       {
