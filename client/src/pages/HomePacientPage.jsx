11c2e0efee95d41d14b0953babc2788064aa39f7
mircogrosch <m1rc0.mg95@gmail.com>
2021-10-29 14:17:28 -0300

    refactor(pages/HomePacientPage.jsx) se agregan componentes para ser visualizados

--- client/src/pages/HomePacientPage.jsx
+++ client/src/pages/HomePacientPage.jsx
@@ -4,14 +4,28 @@ import { useStyles } from "../styles/otroHome/index";
 import SimpleAppBar from "../components/AppBar/SimpleAppBar";
 import Perfil from "../components/homeOtros/Perfil";
 import AppBarIcons from "../components/homeOtros/AppBarIcons";
-
+import ShifsNotificator from "../components/Home/Notificator/ShifsNotificator.jsx";
+import { Grid } from "@mui/material";
+import { AccountBox } from "@mui/icons-material";
+import ContainerCards from '../components/Home/Cards/ContainerCards.jsx'
+const array = [
+  { title: "Mis Profesionales", icon: <AccountBox sx={{ fontSize: 50 }} /> },
+];
 function HomePacientPage() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <SimpleAppBar background={teal[200]} content={<AppBarIcons />} />
-      <Perfil />
+      <Grid container md={12} columnSpacing={4}>
+        <SimpleAppBar background={teal[200]} content={<AppBarIcons />} />
+        <Grid item md={4}>
+          <Perfil />
+        </Grid>
+        <Grid item md={8}>
+          <ShifsNotificator />
+          <ContainerCards cardInfo={array}/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
