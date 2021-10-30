a715bd6afb556fcf29ee31261642c664f4f3f401
IanDiaz27 <iandiaz270601@gmail.com>
2021-10-30 10:13:38 -0300

    refactor(client/src/store/index): modifique la importacion de reducers

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import reducers from "../reducers";
+import reducers from "../reducers/index";
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
