7bc98b992ed5a37bd0afc5743e22ea1aee1878ed
unknown <robolfba@gmail.com>
2021-10-30 00:51:47 -0300

    refactor(reducers/index):se agrega formDocReducer al combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
+import formDocReducer from "./formDocReducer";
 //import reducers here
-const initialState = { 
-       
-} 
-export default combineReducers({ 
-    //add reducers here
-})
+// const initialState = {};
+export default combineReducers({
+  //add reducers here
+  formDocReducer,
+});
