5a1cbd0c739aa280b4e63870c36a78687cd63ef6
unknown <robolfba@gmail.com>
2021-10-30 01:51:12 -0300

    feat(reducers/users) : se crea un estado global para guardar usuarios

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import formDocReducer from "./formDocReducer";
+import users from "./users";
 //import reducers here
 // const initialState = {};
 export default combineReducers({
   //add reducers here
   formDocReducer,
+  users,
 });
