aae0e30b015572f9c697233db0ebcf8563edb923
Alex <fabiansotovillanueva@gmail.com>
2021-10-31 17:24:40 -0300

    Se creo ruta GET que devuelve todas las Obras Sociales

--- api/src/routes/doctor.js
+++ api/src/routes/doctor.js
@@ -1,17 +1,24 @@
 const { Router } = require("express");
 const router = Router();
-const { createDoctor, getDoctor, getDoctors, getPatients} = require("../controllers/doctor.controllers");
+const { createDoctor, getDoctor, getDoctors, getPatient, getPatients} = require("../controllers/doctor.controllers");
 
-router.route("/:id").get(getDoctor);
 
 router.route("/").get(getDoctors);
 
 router.route("/").post(createDoctor);
 
 /*
+Se envia el nombre del paciente y el id del Doctor mediante query
+ej: localhost:3001/doctor/patient?name=Robert&id=id_doctor
+*/
+router.route("/patient").get(getPatient);
+
+/*
 Para traer todos los Pacientes de la lista de Pacientes de un Doctor
 Se envia por params el id del Doctor
 */
 router.route("/patients/:id").get(getPatients);
+
+router.route("/:id").get(getDoctor);
 
 module.exports = router;
