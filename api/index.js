4c630e8c9ac5049083601cdfe36468fbd7f2be05
mircogrosch <m1rc0.mg95@gmail.com>
2021-11-01 01:02:11 -0300

    refactor(api/index.js) se modifica para tener conexion de socket io

--- api/index.js
+++ api/index.js
@@ -2,11 +2,14 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { HealthInsurance, Speciality } = conn.models;
 const xlxs = require("xlsx");
+const http = require('http')
+const socketIO = require('socket.io');
+const cors = require('cors')
 
 const excel_to_json = (dir) => {
   const excel = xlxs.readFile(dir);
   let datos = xlxs.utils.sheet_to_json(excel.Sheets[excel.SheetNames[0]]);
-  return datos;
+  return datos;  
 };
 
 async function addHealthInsurance(datos) {
@@ -37,7 +40,24 @@ conn.sync({ force: true }).then(() => {
   const especialidades = excel_to_json(`${__dirname}/src/especialidades.xlsx`);
   addHealthInsurance(obras_sociales);
   addSpeciality(especialidades);
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // view on console
+
+server.use(cors())
+
+//socket.io
+const server_pp = http.createServer(server); 
+const io = socketIO(server_pp, {
+  path:'/notification',
+  cors:{
+    origin: "http://localhost:3000",
+    methods: ["GET","POST","PUT"]
+  }
+})
+require('./src/controllers/notification')(io)
+  //start server 
+
+const port =3001
+
+  server_pp.listen(port, () => {
+    console.log(`Server is executed on port ${port}` ); // view on console
   });
 });
