5279ee93fbbcb57d911829f07df7101fd38af119
IanDiaz27 <iandiaz270601@gmail.com>
2021-11-01 15:18:55 -0300

    refactor(client/src/components/FiltroSelect/FiltroSelect.jsx) cambios en el filtro

--- client/src/components/FiltroSelect/FiltroSelect.jsx
+++ client/src/components/FiltroSelect/FiltroSelect.jsx
@@ -1,16 +1,18 @@
 import { MenuItem, TextField } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
 import { handleChangeSpecial } from "../Controlers/Controlers";
+import { useDispatch } from "react-redux";
+// import { getSpecialities } from "../../actions";
 
 const MySelect = styled(TextField)({
     backgroundColor: '#80cbc4',
-    width: '400px',
+    width: '500px',
     // height: '50px',
     borderRadius: '3px',
     margin: '30px',
-    border: 'none'
+    marginRight:'270px',
 })
 
 const specialties = [
@@ -21,11 +23,15 @@ const specialties = [
   ];
 
 const FiltroSelect = () =>{
+    // const dispatch = useDispatch()
+    // useEffect(() => {
+    //     dispatch(getSpecialities)
+    // })
       
     const [speciality, setSpeciality] = useState('');
       
     return(
-        <Box sx={{backgroundColor: '#b2dfdb'}}>
+        <Box>
             <MySelect
             select
             color = 'transparent'
@@ -40,7 +46,7 @@ const FiltroSelect = () =>{
                     </MenuItem>
                 ))}
             </MySelect>
-        </Box>
+         </Box>
     )
 }
 
