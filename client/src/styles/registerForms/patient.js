c3e199c6c16e03b1837c356e223c4f9c3402b8e4
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-11-02 19:23:29 -0400

    Estilos Formulario Paciente

---
+++ client/src/styles/registerForms/patient.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+  title: {
+    marginTop: "20px",
+    color: "#878787",
+    textAlign: "center",
+  },
+  input: {
+    borderBottomColor: "green",
+    width: "500px",
+  },
+  button: {
+    width: "500px",
+    height: "3em",
+    fontSize: "20px",
+  },
+});
