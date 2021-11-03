e22b074fff6afcb62eb02fe871f087c7b82c9fc9
mircogrosch <m1rc0.mg95@gmail.com>
2021-11-03 19:35:28 -0300

    refactor(Home/ContainerCards) se modifica para poder reenderizar cualquier valor de carta

--- client/src/components/Home/ContainerCards.jsx
+++ client/src/components/Home/ContainerCards.jsx
@@ -1,62 +1,35 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
-import {
-  EventAvailableOutlined,
-  EventNoteOutlined,
-  AccountBoxOutlined,
-  ListAltOutlined,
-  ForumOutlined,
-} from "@mui/icons-material";
 import Card from "./cards/Card.jsx";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const cardInfo = [
-  {
-    title: "Mis Turnos",
-    icon: <EventAvailableOutlined sx={{ fontSize: 50 }} />,
-  },
-  {
-    title: "Historial Medico",
-    icon: <EventNoteOutlined sx={{ fontSize: 40 }} />,
-  },
-  {
-    title: "Mis Profesionales",
-    icon: <AccountBoxOutlined sx={{ fontSize: 40 }} />,
-  },
-  { title: "Mis Recetas", icon: <ListAltOutlined sx={{ fontSize: 40 }} /> },
-  { title: "Mis Órdenes", icon: <ListAltOutlined sx={{ fontSize: 40 }} /> },
-  { title: "Mensajes", icon: <ForumOutlined sx={{ fontSize: 40 }} /> },
-];
-
-function ContainerCards() {
-  const user = useSelector((state) => state.users);
+function ContainerCards({ cardInfo,bgColor,infoColor }) {
+  const user = useSelector((state) => state.users.users);
   return (
-    <Box sx={{ marginTop: { md: "2em" } }}>
+    <Box sx={{ marginTop: { md: "2em"} }}>
       <Grid container columnSpacing={3} rowSpacing={4}>
-        <Grid item md={4} sm={4} xs={5}>
-          <Card title="Mis Turnos" icon={cardInfo[0].icon} />
-        </Grid>
-        <Grid item md={4} sm={4} xs={5}>
-          <Card title="Historial Medico" icon={cardInfo[1].icon} />
-        </Grid>
-        <Grid item md={4} sm={4} xs={5}>
-          <Link
-            to={`/account/prueba/${user.users.patient.id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <Card title="Mis Profesionales" icon={cardInfo[2].icon} />
-          </Link>
-        </Grid>
-        <Grid item md={4} sm={4} xs={5}>
-          <Card title="Mis Recetas" icon={cardInfo[3].icon} />
-        </Grid>
-        <Grid item md={4} sm={4} xs={5}>
-          <Card title="Mis Órdenes" icon={cardInfo[4].icon} />
-        </Grid>
-        <Grid item md={4} sm={4} xs={5}>
-          <Card title="Mensajes" icon={cardInfo[5].icon} />
-        </Grid>
+        {cardInfo.map((info) =>
+          info.title === "Mis Profesionales" ||
+          info.title === "Mis Pacientes" ? (
+            <Grid item md={4} sm={4} xs={5}>
+              <Link
+                to={
+                  user.patient
+                    ? `/account/profesionales/${user.patient.id}`
+                    : `/account/profesionales/${user.doctor.id}`
+                }
+                style={{textDecoration:"none"}}
+              >
+                <Card title={info.title} icon={info.icon} bgColor={bgColor} color={infoColor} />
+              </Link>
+            </Grid>
+          ) : (
+            <Grid item md={4} sm={4} xs={5}>
+              <Card title={info.title} icon={info.icon} bgColor={bgColor} color={infoColor}/>
+            </Grid>
+          )
+        )}
       </Grid>
     </Box>
   );
