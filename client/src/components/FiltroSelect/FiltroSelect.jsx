2a1a02252ea0760b3c9425b4967451ea04619ae0
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-11-03 21:20:19 -0400

    Estilos Agregados

--- client/src/components/FiltroSelect/FiltroSelect.jsx
+++ client/src/components/FiltroSelect/FiltroSelect.jsx
@@ -1,4 +1,4 @@
-import { MenuItem, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { styled } from "@mui/material/styles";
@@ -7,46 +7,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { getSpecialities } from "../../actions";
 
 const MySelect = styled(TextField)({
-    backgroundColor: '#80cbc4',
-    width: '500px',
-    // height: '50px',
-    borderRadius: '3px',
-    margin: '30px',
-    marginRight:'270px',
-})
+  backgroundColor: "#80cbc4",
+  width: "500px",
+  borderRadius: "3px",
+  margin: "15px",
+  marginRight: "270px",
+});
 
-const FiltroSelect = () =>{
+const FiltroSelect = () => {
+  const dispatch = useDispatch();
+  const [special, setSpecial] = useState("");
 
-    const specialities = useSelector((state) => state.allSpecialities)
-    console.log(specialities)
-    const dispatch = useDispatch()
-    const [special, setSpecial] = useState('')
+  const specialities = useSelector((state) => state.allSpecialities);
 
-    useEffect(() => {
-        dispatch(getSpecialities())
-    },[dispatch])
-      
-    return(
-        <Box>
-            <MySelect
-            select
-            color = 'transparent'
-            label="Especialidades"
-            value={special}
-            SelectProps={{
-                native: true,
-            }}
-            onChange={(e) => handleChangeSpecial(e, special, setSpecial)}
-            // variant='standard'
-            >
-                {specialities.allSpecialities.map((p) => (
-                    <option key={p.id} value={p.id}>
-                        {p.name}
-                    </option>
-                ))}
-            </MySelect>
-         </Box>
-    )
-}
+  useEffect(() => {
+    dispatch(getSpecialities());
+  }, [dispatch]);
 
-export default FiltroSelect
+  return (
+    <Box>
+      <MySelect
+        select
+        color="transparent"
+        label="Especialidades"
+        value={special}
+        SelectProps={{
+          native: true,
+        }}
+        onChange={(e) => handleChangeSpecial(e, special, setSpecial)}
+      >
+        <option disabled>{""}</option>
+        {specialities.allSpecialities.map((p) => (
+          <option key={p.id} value={p.id}>
+            {p.name}
+          </option>
+        ))}
+      </MySelect>
+    </Box>
+  );
+};
+
+export default FiltroSelect;
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,40 +1,36 @@
 import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
-import { IconButton, InputBase,  } from "@material-ui/core";
+import { IconButton, InputBase } from "@material-ui/core";
 import { Search } from "@mui/icons-material";
 import { handleChangeSpecial } from "../Controlers/Controlers";
 
 const MySearchbar = styled(InputBase)({
-    backgroundColor: '#80cbc4',
-    width: '500px',
-    borderRadius: '3px',
-    margin:'30px',
-    padding:'0px 0px 0px 10px'
-})
+  width: "300px",
+  height: "50px",
+  backgroundColor: "#80cbc4",
+  borderRadius: "5px",
+  margin: "15px",
+  padding: "0px 0px 0px 10px",
+});
 
-const SearchBar = () =>{
+const SearchBar = () => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('')
+  const handleSubmit = (e) => {
+    console.log(input);
+  };
+  return (
+    <MySearchbar
+      variant="outlined"
+      placeholder="Buscar"
+      onChange={(e) => handleChangeSpecial(e, input, setInput)}
+      endAdornment={
+        <IconButton onClick={(e) => handleSubmit(e)}>
+          <Search color="#bdbdbd" />
+        </IconButton>
+      }
+    ></MySearchbar>
+  );
+};
 
-
-    const handleSubmit = (e) => {
-        console.log(input)
-    }
-    return(
-        // <Box sx={{backgroundColor: '#b2dfdb'}}>
-            <MySearchbar 
-            variant='outlined' 
-            placeholder='Buscar'
-            onChange = {(e) => handleChangeSpecial(e, input, setInput)}
-            endAdornment={
-                <IconButton 
-                onClick = {(e) => handleSubmit(e)}>
-                    <Search color='#bdbdbd'/>
-                </IconButton>
-            }>
-            </MySearchbar>
-        // </Box> 
-    )
-}
-
-export default SearchBar
+export default SearchBar;
