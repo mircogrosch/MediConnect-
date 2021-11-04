cecf50769bcc6d42b339ac6d6afe5ba15408a914
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-11-03 22:56:35 -0400

    Codigo Limpiado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,7 @@
 import React, { useState } from "react";
-// import { styled } from "@mui/material/styles";
 import { IconButton, InputBase } from "@material-ui/core";
 import { Search } from "@mui/icons-material";
 import { handleChangeSpecial } from "../Controlers/Controlers";
-
-// const MySearchbar = styled(InputBase)({
-//   width: "300px",
-//   height: "50px",
-//   backgroundColor: "#80cbc4",
-//   border: "1px solid grey",
-//   borderRadius: "5px",
-//   margin: "15px",
-//   padding: "0px 0px 0px 10px",
-// });
 
 const SearchBar = (props) => {
   const [input, setInput] = useState("");
