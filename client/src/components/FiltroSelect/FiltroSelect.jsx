c0b487484a485c330d807caaf9239020481b64e8
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-11-03 22:49:46 -0400

    Estilos del componente acomodados

--- client/src/components/FiltroSelect/FiltroSelect.jsx
+++ client/src/components/FiltroSelect/FiltroSelect.jsx
@@ -1,20 +1,11 @@
+import React, { useEffect, useState } from "react";
 import { TextField } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useEffect, useState } from "react";
-import { styled } from "@mui/material/styles";
 import { handleChangeSpecial } from "../Controlers/Controlers";
 import { useDispatch, useSelector } from "react-redux";
 import { getSpecialities } from "../../actions";
 
-const MySelect = styled(TextField)({
-  backgroundColor: "#80cbc4",
-  width: "500px",
-  borderRadius: "3px",
-  margin: "15px",
-  marginRight: "270px",
-});
-
-const FiltroSelect = () => {
+const FiltroSelect = (props) => {
   const dispatch = useDispatch();
   const [special, setSpecial] = useState("");
 
@@ -26,15 +17,18 @@ const FiltroSelect = () => {
 
   return (
     <Box>
-      <MySelect
+      <TextField
         select
-        color="transparent"
+        variant="filled"
         label="Especialidades"
         value={special}
         SelectProps={{
           native: true,
         }}
+        InputProps={{ disableUnderline: true }}
         onChange={(e) => handleChangeSpecial(e, special, setSpecial)}
+        className={props.styles.selectFilter}
+        sx={{ margin: "15px" }}
       >
         <option disabled>{""}</option>
         {specialities.allSpecialities.map((p) => (
@@ -42,7 +36,7 @@ const FiltroSelect = () => {
             {p.name}
           </option>
         ))}
-      </MySelect>
+      </TextField>
     </Box>
   );
 };
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,35 +1,37 @@
 import React, { useState } from "react";
-import { styled } from "@mui/material/styles";
+// import { styled } from "@mui/material/styles";
 import { IconButton, InputBase } from "@material-ui/core";
 import { Search } from "@mui/icons-material";
 import { handleChangeSpecial } from "../Controlers/Controlers";
 
-const MySearchbar = styled(InputBase)({
-  width: "300px",
-  height: "50px",
-  backgroundColor: "#80cbc4",
-  borderRadius: "5px",
-  margin: "15px",
-  padding: "0px 0px 0px 10px",
-});
+// const MySearchbar = styled(InputBase)({
+//   width: "300px",
+//   height: "50px",
+//   backgroundColor: "#80cbc4",
+//   border: "1px solid grey",
+//   borderRadius: "5px",
+//   margin: "15px",
+//   padding: "0px 0px 0px 10px",
+// });
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   const [input, setInput] = useState("");
 
   const handleSubmit = (e) => {
     console.log(input);
   };
   return (
-    <MySearchbar
+    <InputBase
       variant="outlined"
       placeholder="Buscar"
       onChange={(e) => handleChangeSpecial(e, input, setInput)}
+      className={props.styles.searchBar}
       endAdornment={
         <IconButton onClick={(e) => handleSubmit(e)}>
           <Search color="#bdbdbd" />
         </IconButton>
       }
-    ></MySearchbar>
+    ></InputBase>
   );
 };
 
