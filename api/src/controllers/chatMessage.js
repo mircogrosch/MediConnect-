620038cce72e24b3b7d1c77b44ada76313e93287
mircogrosch <m1rc0.mg95@gmail.com>
2021-11-08 12:17:54 -0300

    feat(api/src/controllers/chatMessage.js) se agrega manejador de ruta de chatMessage

--- api/src/controllers/chatMessage.js
+++ api/src/controllers/chatMessage.js
@@ -1,39 +1,73 @@
-const {Message,Person,Conversation} = require('../db.js')
+const { Message, Person, Conversation } = require("../db.js");
 
-const saveMessage = async (dataMessage)=> {
- const newMessage= await Message.create({
-   text:dataMessage.message}); 
-
-   //busca la persona
-   const personSender = await Person.findOne({
-     where:{ 
-       email: dataMessage.sender,
-     }
-   }); 
-   const personReciver = await Person.findOne({
-     where:{
-       email: dataMessage.reciver
-     }
-   }); 
+const getMessage = async (req, res) => {
+  const { dniSender, dniReciver } = req.query;
+  try {
     const conversation = await Conversation.findOne({
-        include: [
-          {
-            model:Person,
-            where: { 
-              dni: personSender.dataValues.dni
-            }
+      include: [
+        {
+          model: Person,
+          where: {
+            dni: dniSender,
           },
-          {
-            model:Person,
-            where: { 
-              dni: personReciver.dataValues.dni
-            }
-          }
-        ]
-    }); 
-    await newMessage.setConversation(conversation)   
-}
+        },
+        {
+          model: Person,
+          where: {
+            dni: dniReciver,
+          },
+        },
+      ],
+    });
+    const messages = await conversation.getMessages();
 
+    res.json({
+      data: messages,
+      status: 200,
+    });
+  } catch(error){
+      res.status(400).send(error)
+  }
+};
+const saveMessage = async (dataMessage) => {
+  const newMessage = await Message.create({
+    text: dataMessage.message,
+  });
+
+  const personSender = await Person.findOne({
+    where: {
+      email: dataMessage.sender,
+    },
+  });
+  const personReciver = await Person.findOne({
+    where: {
+      email: dataMessage.reciver,
+    },
+  });
+  const conversation = await Conversation.findOne({
+    include: [
+      {
+        model: Person,
+        where: {
+          dni: personSender.dataValues.dni,
+        },
+      },
+      {
+        model: Person,
+        where: {
+          dni: personReciver.dataValues.dni,
+        },
+      },
+    ],
+  });
+  await newMessage.setConversation(conversation);
+  await newMessage.setPerson([personSender.dataValues.dni])
+};
+
+/**
+ * REALIZA LA CONEXIÓN CON EL CHAT Y ESCUCHA EVENTOS CORRESPONDIENTES
+ * @param {*} io Recibe una conexión socket con path /messages
+ */
 const SOCKET_CHAT = (io) => {
   io.on("connection", (socket) => {
     console.log(`Connected: ${socket.id}`);
@@ -42,7 +76,7 @@ const SOCKET_CHAT = (io) => {
     });
     socket.on("chat", (data) => {
       const { message, sender, reciver } = data;
-      saveMessage(data)
+      saveMessage(data);
       io.to(sender).to(reciver).emit("reciveChat", data);
     });
   });
@@ -50,4 +84,5 @@ const SOCKET_CHAT = (io) => {
 
 module.exports = {
   SOCKET_CHAT,
+  getMessage
 };
