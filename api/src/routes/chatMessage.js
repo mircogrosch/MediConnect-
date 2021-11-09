89fb22289e3d4ab2c694090ad863cd9a8c9d9aaf
IanDiaz27 <iandiaz270601@gmail.com>
2021-11-09 15:08:37 -0300

    refact(api/src/controllers/chatMessage.js)Cambios en rutas necesarios

--- api/src/routes/chatMessage.js
+++ api/src/routes/chatMessage.js
@@ -3,7 +3,7 @@ const router = Router();
 const {getMessage,getNotificationChat} = require('../controllers/chatMessage')
 
 router.route("/").get(getMessage);
-router.route("/chat").get(getNotificationChat);
+router.route("/notifications").get(getNotificationChat);
 
 
 module.exports = router; 
