3c249052e74881a8f3c8682a4494887bd87a3c6e
mircogrosch <m1rc0.mg95@gmail.com>
2021-11-09 12:35:00 -0300

    feat se agregan controladores para guardar las notificaciones y obtenerlas

--- api/src/controllers/chatMessage.js
+++ api/src/controllers/chatMessage.js
@@ -1,5 +1,43 @@
-const { Message, Person, Conversation } = require("../db.js");
+const { Message, Person, Conversation,Notification } = require("../db.js");
 
+
+
+ const getNotificationChat = async (req, res)=> { 
+      const {dniReciver,type} = req.query
+  try{ 
+    const notifications = await Notification.findAll({
+      where:{
+       personDni: dniReciver,
+       type: type
+      },
+     //  attributes: ['id', 'idDoctor', 'idPatient', 'description', 'personDni']
+    }); 
+    res.send(notifications)
+   }catch{
+     res.status(400).send("NOT FOUND")
+   }
+ }
+
+
+const saveNotificationChat = async (notification)=> { 
+  const new_Notification = await Notification.create(
+    {
+      description: notification.message,
+      idDoctor: notification.idReciver,
+      idPatient: notification.id_patient,
+      type: notification.type
+    },
+    {
+      fields: ["description", "idDoctor", "idPatient"],
+    }
+  );
+  //busco la persona 
+  const person = await Person.findOne({
+    where: { id: notification.dniReciver },
+  });
+  new_Notification.setPerson(person.dataValues.dni);
+
+}
 const getMessage = async (req, res) => {
   const { dniSender, dniReciver } = req.query;
   try {
@@ -70,13 +108,22 @@ const saveMessage = async (dataMessage) => {
  */
 const SOCKET_CHAT = (io) => {
   io.on("connection", (socket) => {
-    console.log(`Connected: ${socket.id}`);
     socket.on("join", (room) => {
       socket.join(room);
     });
     socket.on("chat", (data) => {
-      const { message, sender, reciver } = data;
+      const { sender, reciver } = data;
       saveMessage(data);
+
+      const newNotificationChat = { 
+        message: `${sender} envio un mensaje`,
+        idReciver: data.idReciver,
+        id_patient: data.id_patient,
+        type: data.type,
+        dniReciver: data.dniReciver
+      }
+      io.to(reciver).emit("reciveNotificationChat",newNotificationChat);
+      saveNotificationChat(newNotificationChat)
       io.to(sender).to(reciver).emit("reciveChat", data);
     });
   });
@@ -84,5 +131,6 @@ const SOCKET_CHAT = (io) => {
 
 module.exports = {
   SOCKET_CHAT,
-  getMessage
+  getMessage,
+  getNotificationChat
 };
