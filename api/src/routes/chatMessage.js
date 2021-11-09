979730017639a04efcbfbc8c7625f4800d914aea
mircogrosch <m1rc0.mg95@gmail.com>
2021-11-09 12:36:46 -0300

    feat se agrega nueva ruta para obtener notificaciones

--- api/src/routes/chatMessage.js
+++ api/src/routes/chatMessage.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const router = Router();
-const {getMessage} = require('../controllers/chatMessage')
+const {getMessage,getNotificationChat} = require('../controllers/chatMessage')
 
 router.route("/").get(getMessage);
-
+router.route("/chat").get(getNotificationChat);
 
 
 module.exports = router; 
