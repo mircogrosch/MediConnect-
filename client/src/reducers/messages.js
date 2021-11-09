65b924ef73bfce18c6e52051ffd7f41c1c54bc20
IanDiaz27 <iandiaz270601@gmail.com>
2021-11-09 15:05:00 -0300

    refact(client/src/actions/types.js)Agrego action y reducer de notificaciones de msj

--- client/src/reducers/messages.js
+++ client/src/reducers/messages.js
@@ -1,14 +1,26 @@
 import types from "../actions/types";
 
 const initialState = {
-    chat: []
+    chat: [],
+    notificationChat: []
 }
 
 export const messages = (state=initialState, action) => {
     switch(action.type){
         case types.GET_MESSAGE:
             return{
+                ...state,
                 chat: action.payload
+            }
+        case 'SAVE_NOTIFICATION_CHAT':
+            return{
+                ...state,
+                notificationChat: [...state.notificationChat, action.payload]
+            }
+        case types.GET_NOTIFICATIONS_MESSAGE:
+            return{
+                ...state,
+                notificationChat: action.payload
             }
         default:
             return state
