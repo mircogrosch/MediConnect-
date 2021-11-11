3c0477e99319a426ae39a118e6e9029525580427
Dhalguin Hernandez <dhalguinjesus21nov@gmail.com>
2021-11-11 18:40:05 -0400

    Se agrega funcionalidad para eliminar dato registrado

--- client/src/components/MedicalHistoryDoctor/cardInfo/DiseaseInfo.jsx
+++ client/src/components/MedicalHistoryDoctor/cardInfo/DiseaseInfo.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Box, Typography, Button } from "@mui/material";
+import swal from "sweetalert";
+import { Box, Typography, Button, IconButton } from "@mui/material";
+import { HighlightOff } from "@mui/icons-material";
 import { teal, grey } from "@mui/material/colors";
 
 function DiseaseInfo({ setDisplayed, patientId }) {
@@ -17,12 +19,46 @@ function DiseaseInfo({ setDisplayed, patientId }) {
     setDiseases(response.data.data);
   };
 
+  const handleDelete = async (diseaseId, patientId) => {
+    try {
+      await axios.delete(
+        `http://localhost:3001/patient/disease/${patientId}?idDisease=${diseaseId}`
+      );
+
+      swal({
+        icon: "success",
+        title: "Enfermedad descartada",
+        timer: 1500,
+      });
+
+      await getDiseases(patientId);
+    } catch (error) {
+      alert(error);
+      swal({
+        icon: "error",
+        title: "Server Error",
+        timer: 2000,
+      });
+    }
+  };
+
   return (
     <Box sx={{ height: "100%", position: "relative" }}>
       {diseases.length > 0 && (
         <Box sx={{ height: "45vh", overflowY: "scroll" }}>
           {diseases.map((disease) => (
-            <Box key={disease.id}>
+            <Box key={disease.id} sx={{ position: "relative" }}>
+              {/* <IconButton
+                sx={{ position: "absolute", top: "5px", right: "5px" }}
+              >
+                <Edit />
+              </IconButton> */}
+              <IconButton
+                onClick={() => handleDelete(disease.id, disease.patientId)}
+                sx={{ position: "absolute", top: "0", right: "0" }}
+              >
+                <HighlightOff sx={{ fontSize: "1.2em" }} />
+              </IconButton>
               <Typography variant="h6" color={grey[800]}>
                 {disease.name}
               </Typography>
