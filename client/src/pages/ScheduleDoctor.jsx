89a08d9963302de8829d617f49d34483fe76cf0b
unknown <robolfba@gmail.com>
2021-11-12 13:46:45 -0300

    Se reemplaza el componente MaterialTable porque traia conflictos en la ejecucion

--- client/src/pages/ScheduleDoctor.jsx
+++ client/src/pages/ScheduleDoctor.jsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from "react";
 import PrimarySearchAppBar from "../components/Notification/AppBarNoti";
-import MaterialTable from "material-table";
 import { Typography, Button, Grid } from "@mui/material";
 import { teal, grey } from "@mui/material/colors";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
 
 const columnas = [
-  { title: "Fecha y hora", field: "date" },
-  { title: "Nombre", field: "patient.person.name" },
-  { title: "Apellido", field: "patient.person.lastname" },
-  { title: "Estado de pago", field: "payment_status" },
+  { name: "Fecha", selector: "date", sortable: true },
+  { name: "Hora", selector: "hour_long", sortable: true },
+  { name: "Nombre", selector: "patient.person.name", sortable: true },
+  { name: "Apellido", selector: "patient.person.lastname", sortable: true },
+  { name: "Estado de pago", selector: "payment_status", sortable: true },
 ];
-
 const ScheduleDoctor = (props) => {
   const [data, setData] = useState([]);
 
@@ -21,7 +22,6 @@ const ScheduleDoctor = (props) => {
       `${URL}/doctor/appointment/${props.match.params.id}`
     );
     setData(response.data.data);
-    console.log(response.data.data);
   };
 
   useEffect(() => {
@@ -39,6 +39,7 @@ const ScheduleDoctor = (props) => {
         rowSpacing={1}
         style={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
         }}
@@ -46,30 +47,41 @@ const ScheduleDoctor = (props) => {
         <Typography align={"center"} variant="h5" style={{ color: grey[700] }}>
           Lista de turnos pendientes
         </Typography>
-        <MaterialTable
-          columns={columnas}
-          data={data}
-          title={""}
+        <Grid
           style={{
-            maxWidth: "80%",
-            background: teal[50],
-          }}
-          components={{
-            Pagination: "disabled",
-          }}
-        />
-        <Button
-          variant="contained"
-          sx={{
             marginTop: "1em",
-            fontSize: "14px",
             width: "80%",
-            height: "50px",
-            background: teal[900],
+            background: grey[100],
+            border: "solid 0.0005px ",
+            borderColor: teal[100],
+            fontFamily: "Roboto",
           }}
         >
-          Configurar agenda
-        </Button>
+          <DataTable
+            columns={columnas}
+            data={data}
+            title={""}
+            fixedHeader
+            fixedHeaderScrollHeight="400px"
+          />
+        </Grid>
+        <Link
+          to={`/account/appointment-config/${props.match.params.id}`}
+          style={{ textDecoration: "none", width: "80%" }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              marginTop: "1em",
+              fontSize: "14px",
+              width: "100%",
+              height: "50px",
+              background: teal[900],
+            }}
+          >
+            Configurar agenda
+          </Button>
+        </Link>
       </Grid>
     </>
   );
