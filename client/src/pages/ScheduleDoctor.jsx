3ab32ad56a56a0f730b256829f12c89ff8f7ca17
unknown <robolfba@gmail.com>
2021-11-13 18:24:31 -0300

    Se agrega comentario para comunicar al usuario cuando no hay turnos pendientes a mostrar en la tabla

--- client/src/pages/ScheduleDoctor.jsx
+++ client/src/pages/ScheduleDoctor.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PrimarySearchAppBar from "../components/Notification/AppBarNoti";
-import { Typography, Button, Grid } from "@mui/material";
+import { Typography, Button, Grid, Box } from "@mui/material";
 import { teal, grey } from "@mui/material/colors";
 import axios from "axios";
 import { Link } from "react-router-dom";
@@ -27,62 +27,95 @@ const ScheduleDoctor = (props) => {
   useEffect(() => {
     getAppointments();
   }, []);
-
+  console.log("data ", data);
   return (
     <>
-      <Grid>
+      <Box sx={{ background: grey[50] }}>
         <PrimarySearchAppBar />
-      </Grid>
-      <Grid
-        container
-        height="100vh"
-        rowSpacing={1}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography align={"center"} variant="h5" style={{ color: grey[700] }}>
-          Lista de turnos pendientes
-        </Typography>
-        <Grid
-          style={{
-            marginTop: "1em",
-            width: "80%",
-            background: grey[100],
-            border: "solid 0.0005px ",
-            borderColor: teal[100],
-            fontFamily: "Roboto",
+        <Box
+          sx={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <DataTable
-            columns={columnas}
-            data={data}
-            title={""}
-            fixedHeader
-            fixedHeaderScrollHeight="400px"
-          />
-        </Grid>
-        <Link
-          to={`/account/appointment-config/${props.match.params.id}`}
-          style={{ textDecoration: "none", width: "80%" }}
-        >
-          <Button
-            variant="contained"
-            sx={{
-              marginTop: "1em",
-              fontSize: "14px",
-              width: "100%",
-              height: "50px",
-              background: teal[900],
+          <Grid
+            container
+            height="70vh"
+            rowSpacing={1}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            Configurar agenda
-          </Button>
-        </Link>
-      </Grid>
+            <Typography
+              align={"center"}
+              variant="h4"
+              style={{ color: grey[700] }}
+            >
+              Lista de turnos
+            </Typography>
+            {data.length ? (
+              <Grid
+                style={{
+                  marginTop: "1em",
+                  width: "80%",
+                  background: grey[100],
+                  border: "solid 0.0005px ",
+                  borderColor: teal[100],
+                  fontFamily: "Roboto",
+                }}
+              >
+                <DataTable
+                  columns={columnas}
+                  data={data}
+                  title={""}
+                  fixedHeader
+                  fixedHeaderScrollHeight="400px"
+                />
+              </Grid>
+            ) : (
+              <Grid
+                // backgroundColor="red"
+                height="30vh"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  align={"center"}
+                  variant="h6"
+                  style={{ color: grey[700] }}
+                >
+                  No tiene ningún turno pendiente
+                </Typography>
+              </Grid>
+            )}
+
+            <Link
+              to={`/account/appointment-config/${props.match.params.id}`}
+              style={{ textDecoration: "none", width: "80%" }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  marginTop: "1em",
+                  fontSize: "14px",
+                  width: "100%",
+                  height: "50px",
+                  background: teal[900],
+                }}
+              >
+                Configurar agenda
+              </Button>
+            </Link>
+          </Grid>
+        </Box>
+      </Box>
     </>
   );
 };
